884e0a99fab6f00ab5627f42624f1b6959440bc9
IanConroy <IanMylesConroy@Gmail.com>
2023-12-01 12:26:51 -0800

    added element, but lacks style; will return

--- js-file.js
+++ js-file.js
@@ -5,3 +5,8 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
 container.appendChild(content);
+
+const redPara = document.createElement("p");
+redPara.textContent = "Hey I'm red!";
+document.body.appendChild(redPara);
+document.getElementsByName(redPara).style.color = "red";
