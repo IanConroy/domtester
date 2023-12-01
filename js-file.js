ea15753d28ed0e5460f504e03233cf6fe98ab809
IanConroy <IanMylesConroy@Gmail.com>
2023-12-01 12:37:58 -0800

    added blue h3 and div with an h1 and p within it per TOP

--- js-file.js
+++ js-file.js
@@ -10,3 +10,18 @@ const redPara = document.createElement("p");
 redPara.textContent = "Hey I'm red!";
 document.body.appendChild(redPara);
 redPara.style.color = "red";
+
+const blueH3 = document.createElement("h3");
+blueH3.textContent = "I'm a blue h3!";
+document.body.appendChild(blueH3);
+blueH3.style.color = "blue";
+
+const newDiv = document.createElement("div");
+
+const newH1 = document.createElement("h1");
+newH1.textContent = "I'm in a div";
+const newP = document.createElement("p");
+newP.textContent = "ME TOO!";
+newDiv.appendChild(newH1);
+newDiv.appendChild(newP);
+document.body.appendChild(newDiv);
